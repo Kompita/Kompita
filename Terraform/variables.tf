5abda3aafa7ed67332a30a8ac9b2737e9971d7c6
Vanessa Ruiz Robledinos <vruizrob@emeal.nttdata.com>
2021-12-16 08:50:24 +0100

    a

---
+++ Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  type        = string
+  description = "f27f0ec9-d16f-4dbd-a117-0402dfe43995"
+}
+
+variable "client_id" {
+  type        = string
+  description = "4b69e8c9-a035-43be-a219-fd698fd14c9e"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "eiu7Q~yiAuecdHlHuOk0xECBueCOIC.hVakdO"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "60e9e929-ca81-4267-84e6-d10cf0fa8c81"
+} 
--- src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>OOM!</title>
-</head>
-<body style="text-align: center">
-<h1>OOM!</h1>
-</body>
-</html>
